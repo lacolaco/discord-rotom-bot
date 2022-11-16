bcee90e1a5992a68c41dd94bb478c5cbbe41178a
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-11-16 18:30:51 +0900

    fix: improve logs

--- src/pokeinfo/search.ts
+++ src/pokeinfo/search.ts
@@ -10,11 +10,13 @@ import iconv from 'iconv-lite';
 export async function searchURLByName(name: string): Promise<string | null> {
   // TODO: ポケ徹が対応したらSVに移行する
   const pokemonDataPageURL = 'https://yakkun.com/swsh/stats_list.htm';
-
+  console.log(`Requesting ${pokemonDataPageURL}`);
   const response = await request(pokemonDataPageURL, { method: 'GET' });
+  console.log(`Response status: ${response.statusCode}`);
   // yakkun.com のcharsetが EUC-JP なので UTF-8 に変換する
   const body = await response.body.arrayBuffer().then((buf) => iconv.decode(Buffer.from(buf), 'EUC-JP'));
 
+  console.log(`Searching for an URL of ${name}`);
   const dom = new JSDOM(body);
   const linkAnchor = queryByRole<HTMLAnchorElement>(dom.window.document.body, 'link', { name });
   if (!linkAnchor || !linkAnchor.href) {
