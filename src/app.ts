9106598690ec1d74d1c052d0f64b65ba50da68c1
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-11-16 18:03:58 +0900

    fix: rescue error in catch

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,9 @@ client.on(Events.InteractionCreate, async (interaction) => {
     await command.execute(interaction);
   } catch (error) {
     console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    interaction
+      .reply({ content: 'There was an error while executing this command!', ephemeral: true })
+      .catch((error) => console.error(error));
   }
 });
 
