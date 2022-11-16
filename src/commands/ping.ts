221bd84ef1a1650e458a5fe3693d172217ad08c5
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-11-16 18:28:58 +0900

    fix: test deferred with ping-pong

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,6 +3,8 @@ import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
 export default {
   data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
   async execute(interaction: ChatInputCommandInteraction) {
-    await interaction.reply({ content: 'Pong!', ephemeral: true });
+    await interaction.deferReply({ ephemeral: true });
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await interaction.editReply({ content: 'Pong!' });
   },
 };
